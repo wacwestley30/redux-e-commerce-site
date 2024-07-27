58f51623be66b9d3fdd7fd43edea73aa097668f7
wacwestley30 <westleycervantes@gmail.com>
2024-07-27 06:08:22 -0400

    Basic server is running

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,18 @@
 const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+    }
 
+    type Auth {
+        token: String!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
 `;
 
 module.exports = typeDefs;
